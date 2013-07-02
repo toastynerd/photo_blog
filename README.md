4fe5bef00d1b70cb8b08b4997246811133d490f3
tylermorgan86 <tylermorgan86@gmail.com>
2013-07-02 12:24:28 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Photoblog App a Day
+===========================
+
+A photoblog that uses resque to upload photos in a background task.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photoblog::Application.config.secret_key_base = '817e140f85105d8e25577238e720fb4439da7e106e159e886568518372d98c1f4d80935406b2ce1953a49c19bcce7d81fae44c79429ee4e870dad3d77e6f4000'
